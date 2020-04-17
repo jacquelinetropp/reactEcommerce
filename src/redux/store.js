152788de08b90b8e56199bc3fb7d0ed2bc6214d4
travelingtropp <48297118+travelingtropp@users.noreply.github.com>
2020-04-17 12:02:51 -0500

    selector of directory not working

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReduce from "./root-reducer";
 
 const middlewares = [logger];
 
-const store = createStore(rootReduce, applyMiddleware(...middlewares));
-
-export default store;
+export const store = createStore(rootReduce, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
