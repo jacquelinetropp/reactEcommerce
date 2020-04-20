0d2ca894268d7af5091645a0d8dd20373f1b4a51
travelingtropp <48297118+travelingtropp@users.noreply.github.com>
2020-04-20 09:13:49 -0500

    Basic completion of application

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,12 @@ import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
-import rootReduce from "./root-reducer";
+import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-export const store = createStore(rootReduce, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
 export const persistor = persistStore(store);
+
+export default { store, persistStore };
